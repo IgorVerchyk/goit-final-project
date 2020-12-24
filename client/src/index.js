00afbe874de697c9eeffdb908c0cc4233c4c64fa
Maksim Skipin <skipysmaks@gmail.com>
2020-12-24 20:12:13 +0200

    added npm node-sass normalize. transferred file BD json-server in src

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,9 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './components/App';
 import { store, persistor } from './redux/store';
+
+import 'modern-normalize/modern-normalize.css';
+
 console.log(store.getState());
 
 ReactDOM.render(
