a5034850c9b2a75a067866595194c65924aaa535
Sergey Shapoval <shapa92sergey@gmail.com>
2020-12-26 18:12:46 +0200

    Add logic reg auth

--- backend/src/api/auth/auth.js
+++ backend/src/api/auth/auth.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const guard = require("../../helpers/guard");
+const controllerUsers = require("../../controllers/auth");
 
+router.get("/current", controllerUsers.current);
+router.post("/register", controllerUsers.reg);
+router.post("/login", controllerUsers.login);
+router.post("/logout", guard, controllerUsers.logout);
+
+module.exports = router;
