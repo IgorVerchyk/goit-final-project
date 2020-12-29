14c6b82b44713142844086067d5eb2fdda4e8adc
IgorVerchyk <sabyr.ivua@gmail.com>
2020-12-29 20:55:03 +0200

    resolved conflict in back branch

--- backend/src/api/projects/projects.js
+++ backend/src/api/projects/projects.js
@@ -1,21 +1,7 @@
-// const express = require("express");
-// const usersController = require("../../controllers/projects");
-// const router = express.Router();
-// const guard = require("../../helpers/guard");
-// const { createAccountLimiter } = require("../../helpers/reate-limit");
-// const upload = require("../../helpers/multer");
+const projectsRouter = express.Router();
 
-// router.get("/current", guard, usersController.current);
-// router.get("/verify/:token", usersController.verify);
+projectsRouter.post("/", ProjectController.createProject);
 
-// router.post("/registration", createAccountLimiter, usersController.reg);
-// router.post("/login", usersController.login);
-// router.post("/logout", guard, usersController.logout);
-// router.patch(
-//   "/avatars",
-//   guard,
-//   upload.single("avatar"),
-//   usersController.avatars
-// );
+projectsRouter.delete("/:projectId", ProjectController.removeProject);
 
-// module.exports = router;
+module.exports = projectsRouter;
