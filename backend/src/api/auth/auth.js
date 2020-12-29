28a31d6bd9c81da0f9303f32ce75416e2b096a44
Igor Verchyk <51333575+IgorVerchyk@users.noreply.github.com>
2020-12-29 09:18:52 +0200

    Revert "added projects creation and removal"

--- backend/src/api/auth/auth.js
+++ backend/src/api/auth/auth.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const guard = require("../../helpers/guard");
+const controllerUsers = require("../../controllers/auth");
 
+router.get("/current", controllerUsers.current);
+router.post("/registration", controllerUsers.reg);
+router.post("/login", controllerUsers.login);
+router.post("/logout", guard, controllerUsers.logout);
+
+module.exports = router;
--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 const uriDb = process.env.URI_DB;
 
 const db = mongoose.connect(uriDb, {
-  promiseLibrary: global.Promise,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useNewUrlParser: true,
@@ -26,6 +25,7 @@ mongoose.connection.on("disconnected", (err) => {
 
 process.on("SIGINT", () => {
   mongoose.connection.close(() => {
+    console.log("Connection for DB disconnected and app terminated");
     process.exit(1);
   });
 });
