8a76fbad758a4a4f957fd2bcb6063943f0fc0c6f
Alex Khomenko <sasvsha@gmail.com>
2020-12-29 15:43:16 +0100

    test

--- backend/src/api/projects/projectsRoutes.js
+++ backend/src/api/projects/projectsRoutes.js
@@ -3,6 +3,7 @@ const ProjectController = require("../../controllers/projects");
 const projectsRouter = express.Router();
 
 projectsRouter.post("/", ProjectController.createProject);
+
 projectsRouter.delete("/:projectId", ProjectController.removeProject);
 
 module.exports = projectsRouter;
