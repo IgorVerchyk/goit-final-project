27619f5452dbf1e2303e6d6a3f8d292198958f87
IgorVerchyk <sabyr.ivua@gmail.com>
2021-01-03 19:19:13 +0200

    try to fix validation by email

--- backend/src/repository/users.js
+++ backend/src/repository/users.js
@@ -1,4 +1,4 @@
-const User = require('../schemas/user');
+const User = require("../schemas/user");
 
 class UsersRepository {
   constructor() {
@@ -14,11 +14,6 @@ class UsersRepository {
     const result = await this.model.findOne({ email });
     return result;
   }
-  async findByToken(token) {
-    const result = await this.model.findOne({ verifyToken: token });
-    return result;
-  }
-
   async findByToken(token) {
     const result = await this.model.findOne({ verifyToken: token });
     return result;
@@ -40,7 +35,7 @@ class UsersRepository {
         $push: {
           projects: { projectId, title, descr },
         },
-      },
+      }
     );
     return result;
   }
@@ -51,7 +46,7 @@ class UsersRepository {
       { _id: id },
       {
         $pull: { projects: { _id: projectId } },
-      },
+      }
     );
     return result;
   }
