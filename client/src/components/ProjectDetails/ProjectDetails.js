fd71ff97ab9033a07a7850e3211f542c8b99f6d2
tamara-f <filatovatam@ukr.net>
2021-01-05 18:05:07 +0200

    created link to script details on projectdetails page

--- client/src/components/ProjectDetails/ProjectDetails.js
+++ client/src/components/ProjectDetails/ProjectDetails.js
@@ -1,24 +1,48 @@
-import { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
-import React, { useCallback } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import Modal from '../Modal/Modal';
 
 export default function ProjectDetails() {
   const { projectId } = useParams();
-  const proj = useSelector(state => state.projects);
-  console.log(proj);
-  // find(i => i._id === projectId));
-  // console.log(proj.items.iterable.find(item => item._id === { projectId }));
-  // const [project, setProject] = useState(null);
 
-  // useEffect(() => {
-  //  fetchProjectDetails(id).then(setProject);
-  // }, [id]);
+  const projects = useSelector(state => state.projects.items);
+  const project = projects.find(item => item.id === projectId);
+
+  const [isModal, setisModal] = useState(false);
+
+  const toggleModal = () => {
+    const toggledIsOpen = isModal ? false : true;
+    setisModal(toggledIsOpen);
+  };
 
   return (
     <>
-      <h2>details of project id: {projectId}</h2>
+      <h2>Project: {project.projectName}</h2>
+      <div>Додати людей</div>
+      {!isModal ? (
+        <>
+          <button className="addNew" onClick={toggleModal}>
+            Створити спринт
+          </button>
+        </>
+      ) : (
+        <Modal closeModal={toggleModal} children={''} />
+      )}
+
+      <ul>
+        {project.sprints.map(sprint => (
+          <li key={sprint.id}>
+            <Link to={`/${projectId}/${sprint.id}`}>
+              <h2>{sprint.description}</h2>
+              <p>{sprint.startDate}</p>
+              <p>{sprint.finDate}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
