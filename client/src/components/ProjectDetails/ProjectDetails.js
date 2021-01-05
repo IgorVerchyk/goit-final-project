2242b3326842555bdde80ef0c3047bb7af39bbea
Sergey Shapoval <shapa92sergey@gmail.com>
2021-01-05 11:56:36 +0200

    fixed

--- client/src/components/ProjectDetails/ProjectDetails.js
+++ client/src/components/ProjectDetails/ProjectDetails.js
@@ -1,19 +1,19 @@
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-// import { FetchProjectDetails } from '../../';
-// import s from './ProjectDetails.module.css';
+// import { useState, useEffect } from 'react';
+// import { useParams } from 'react-router-dom';
+// // import { FetchProjectDetails } from '../../';
+// // import s from './ProjectDetails.module.css';
 
-export default function RegisterView() {
-  const { projectId } = useParams();
-  const [project, setProject] = useState(null);
+// export default function RegisterView() {
+//   const { projectId } = useParams();
+//   const [project, setProject] = useState(null);
 
-  // useEffect(() => {
-  //  fetchProjectDetails(id).then(setProject);
-  // }, [id]);
+//   // useEffect(() => {
+//   //  fetchProjectDetails(id).then(setProject);
+//   // }, [id]);
 
-  return (
-    <>
-      <h2>details of project id: {projectId}</h2>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <h2>details of project id: {projectId}</h2>
+//     </>
+//   );
+// }
