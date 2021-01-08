a88bc45105b7a6aa92a98ff7e673c3dd1611373a
IgorVerchyk <sabyr.ivua@gmail.com>
2021-01-09 01:16:27 +0200

    package-lock restored

--- client/src/components/SectionScroll/SectionScroll.js
+++ client/src/components/SectionScroll/SectionScroll.js
@@ -1,5 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default function SectionScroll(arr, children) {
-  return <div></div>;
+import SectionScrollItem from './SectionScrollItem';
+
+import s from './SectionScroll.module.scss';
+
+export default function SectionScroll(arr) {
+  const [index, setIndex] = useState(0);
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    setList(
+      arr.map(
+        project =>
+          arr.indexOf(project) >= index && arr.indexOf(project) < index + 6,
+      ),
+    );
+  }, [index, arr]);
+
+  const incrementIndex = () => {
+    setIndex(index + 1);
+  };
+  const decrementIndex = () => {
+    setIndex(index - 1);
+  };
+
+  return (
+    <div className={s.scrollContainer}>
+      <button className={s.scrollButtom} onClick={decrementIndex}>
+        ...
+      </button>
+      <ul className={s.scrollList}>
+        {list.map(listItem => (
+          <SectionScrollItem key={listItem.objectId} {...listItem} />
+        ))}
+      </ul>
+      <button className={s.scrollButtom} onClick={incrementIndex}>
+        ...
+      </button>
+    </div>
+  );
 }
--- client/src/redux/projects/projectsOperations.js
+++ client/src/redux/projects/projectsOperations.js
@@ -15,8 +15,8 @@ import {
   removeProjectError,
 } from './projectsActions';
 
- const baseURL = 'https://project-manager-goit20.herokuapp.com/api/projects/';
-// const baseURL = 'http://localhost:3456/api/projects';
+//const baseURL = 'https://project-manager-goit20.herokuapp.com/api/projects/';
+const baseURL = 'http://localhost:3456/api/projects';
 
 const fetchProjects = id => async dispatch => {
   dispatch(fetchProjectsRequest());
