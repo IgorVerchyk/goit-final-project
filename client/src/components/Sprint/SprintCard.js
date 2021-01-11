be5991876e8c1b0aaa588713f51f22d999e51778
tamara-f <filatovatam@ukr.net>
2021-01-11 09:38:15 +0200

    fixed link to sprint details

--- client/src/components/Sprint/SprintCard.js
+++ client/src/components/Sprint/SprintCard.js
@@ -2,10 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function SprintCard(sprint) {
-  console.log(sprint);
   const { routeTo, id, description, startDate, finDate } = sprint;
   return (
-    <li>
+    <li key={id}>
       <Link to={routeTo} className="link" id={id}>
         <h2>{description}</h2>
         <p>{startDate}</p>
